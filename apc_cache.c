5bf6033aa2ecddf7b40899ead9dfe5430af4345f
Antony Dovgal <tony@daylessday.org>
2015-10-07 13:40:07 +0300

    apcu_inc/dec should create missing cache entries

--- apc_cache.c
+++ apc_cache.c
@@ -989,6 +989,7 @@ PHP_APCU_API apc_cache_entry_t* apc_cache_exists(apc_cache_t* cache, char *strke
 PHP_APCU_API zend_bool apc_cache_update(apc_cache_t* cache, char *strkey, zend_uint keylen, apc_cache_updater_t updater, void* data TSRMLS_DC)
 {
     apc_cache_slot_t** slot;
+    apc_cache_entry_t tmp_entry;
 	
     zend_bool retval = 0;
     zend_ulong h, s;
@@ -1045,9 +1046,17 @@ PHP_APCU_API zend_bool apc_cache_update(apc_cache_t* cache, char *strkey, zend_u
 		/* set next slot */
         slot = &(*slot)->next;
 	}
-	
+
 	/* unlock header */
 	APC_UNLOCK(cache->header);
+
+	/* failed to find matching entry, create it */
+	ZVAL_LONG(&tmp_entry.val, 0);
+	updater(cache, &tmp_entry, data);
+
+	if(apc_cache_store(cache, key, &tmp_entry.val, 0, 0)) {
+		return 1;
+	}
 
     return 0;
 }
